124cca74f9dc968e105bbaa6aaf62dfb3af58325
Jeremy Laskin <jlaskin@seas.upenn.edu>
2014-03-17 23:26:03 -0400

    Validations HW

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,8 @@
 class Recommendation < ActiveRecord::Base
   belongs_to :user
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, length: {maximum: 1000}
+  validates :title, length: {maximum: 50}
+  validates :title, uniqueness: true
 end
